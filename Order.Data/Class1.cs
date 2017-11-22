6f81d7dea8b91ab4720bcb6fa521255058832717
Michael Moody <dcmoods@gmail.com>
2017-11-22 14:23:33 -0500

    added order projects

---
+++ Order.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Order.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Order.Domain/InvoiceItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Order.Domain
+{
+    public class InvoiceItem
+    {
+        [Key]
+        public int InvoiceItemId { get; set; }
+        public int InvoiceId { get; set; }
+
+        public Invoice Invoice { get; set; }
+        public StockItem StockItem { get; set; }
+
+    }
+}
